d7ab1142ba5dc481fe431d3ea91b48cadcbe00ce
Somesh Arora <somesh.arora@microsoft.com>
2021-12-04 19:49:51 -0800

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Somesh Arora on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
